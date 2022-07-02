b8456351f3e35ffea32cd795cd5ab2512ab40980
Juan Luis <juanluiscr27@hotmail.com>
2022-07-02 17:33:57 -0400

    Main and GUI

---
+++ src/ui/SearchPanel.java
@@ -0,0 +1,9 @@
+package ui;
+
+import javax.swing.JPanel;
+
+public class SearchPanel extends JPanel {
+        /* Screen Resolution 1280x720
+        Screen Width: 1280 pixels
+        Screen Height: 720 pixels */
+}
