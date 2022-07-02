93a18653d66ed4b2350fdc438098a225143ff515
Juan Luis <juanluiscr27@hotmail.com>
2022-07-02 19:56:33 -0400

    Database Connection test

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,8 @@
 import ui.MainFrame;
-import ui.SearchPanel;
-
 public class Main {
-
     public static void main(String[] args) {
         /* This code creates a Java Frame */
         MainFrame mainFrame = new MainFrame();  // Instantiate a MainFrame (JFrame) object
-        SearchPanel searchPanel = new SearchPanel();
-        mainFrame.add(searchPanel);
         mainFrame.setVisible(true);
     }
 }
