516cf89ab186f6aa8fedbf4a2d5f423337d6fe70
Juan Luis <juanluiscr27@hotmail.com>
2022-08-03 22:14:51 -0400

    -m Search customers and search account functions to update SearchPanel tables

--- src/ui/SearchPanel.java
+++ src/ui/SearchPanel.java
@@ -3,7 +3,9 @@ package ui;
 import entity.*;
 
 import javax.swing.*;
+import javax.swing.table.*;
 import java.awt.*;
+import java.util.*;
 
 
 public class SearchPanel extends JPanel {
@@ -18,11 +20,17 @@ public class SearchPanel extends JPanel {
     JTable accountTable;
     final Color LIGHT_CYAN = new Color(224, 240, 255);  // Creates a color based on an RGB code
     Agent bankAgent;
+    Return result;
+    ArrayList<Customer> resultCustomers;
+    ArrayList<Account> resultAccounts;
     MainFrame mainFrame;
     public SearchPanel(MainFrame mainFrame) {
         super(); // Initializes a JPanel class instance
         this.mainFrame = mainFrame;
         this.bankAgent = mainFrame.bankAgent;
+        result = new Return();
+        resultCustomers = new ArrayList<Customer>();
+        resultAccounts = new ArrayList<Account>();
         this.setLayout(null);
         this.setBackground(LIGHT_CYAN); // Change the panel background color
         getAgentIdLabel();
@@ -62,22 +70,41 @@ public class SearchPanel extends JPanel {
         // Search action for the button click event
         searchButton.addActionListener(event -> {
             /* Search customers or accounts */
-
+            System.out.println("\n\n***** SEARCH TEST *****");
+            bankAgent.agentSearchCustomers(searchTextField.getText(), resultCustomers, result);
+            if (Objects.equals(result.getCode(), "00")) {
+                System.out.println("\nCUSTOMERS:");
+                for (Customer resultCustomer : resultCustomers) {
+                    System.out.print(resultCustomer.getFirstName() + "\n");
+                }
+                updateCustomerTable();
+            }
+            else {
+                System.out.println(result.getCode() + " - " + result.getMessage());
+            }
+            result = new Return();
+            bankAgent.agentSearchAccounts(searchTextField.getText(), resultAccounts, result);
+            if (Objects.equals(result.getCode(), "00")) {
+                System.out.println("\nACCOUNTS:");
+                for (Account resultAccount : resultAccounts) {
+                    System.out.print(resultAccount.getAccNumber() + " " + resultAccount.getAccType() + "\n");
+                }
+                updateAccountTable();
+            }
+            else {
+                System.out.println(result.getCode() + " - " + result.getMessage());
+            }
         });
     }
 
     private void getCustomerTable() {
         String [] columnNames = {
-                "Customer ID", "First Name", "Last Name", "Address", "Phone Number", "Email"
+                "Customer ID", "First Name", "Last Name", "Address", "Phone Number", "Email", "Creation Date"
         };
-        String[][] data = {
-                {"1", "Nancy", "Sallings", "4291 Derek Road", "394-391-7522", "nsallings0@prnewswire.com"},
-                {"2", "Lebbie", "Gimbrett", "315 Bunker Hill Alley", "647-399-3239", "lgimbrett1@huffingtonpost.com"},
-                {"3", "Jake", "Fitzmaurice", "108 Artisan Alley", "417-371-5483", "jfitzmaurice2@virginia.edu"},
-                {"4", "Lil", "Weld", "45 Basil Crossing", "954-551-8848", "lweld3@buzzfeed.com"},
-                {"5", "Anita", "Phittiplace", "8 Dapin Street", "540-128-2637", "aphittiplace4@latimes.com"}
-        };
-        customerTable = new JTable(data,columnNames);
+        String[][] data = new String[0][];
+        TableModel tableModel = new DefaultTableModel(data, columnNames);
+
+        customerTable = new JTable(tableModel);
         customerTable.setPreferredScrollableViewportSize(new Dimension(500,100));
         customerTable.setFillsViewportHeight(true);
     }
@@ -89,19 +116,33 @@ public class SearchPanel extends JPanel {
         this.add(customerScrollPane, null);
     }
 
+    private void updateCustomerTable() {
+        DefaultTableModel tableModel = (DefaultTableModel) customerTable.getModel();
+        tableModel.setRowCount(0);
+        for (Customer customer : resultCustomers) {
+            String[] data = new String[tableModel.getColumnCount()];
+            data[0] = String.valueOf(customer.getCustomerId());
+            data[1] = customer.getFirstName();
+            data[2] = customer.getLastName();
+            data[3] = customer.getAddress();
+            data[4] = customer.getPhoneNumber();
+            data[5] = customer.getEmail();
+            data[6] = String.valueOf(customer.getCreationDate());
+            tableModel.addRow(data);
+        }
+        customerTable.setModel(tableModel);
+        customerTable.repaint();
+    }
+
     private void getAccountTable() {
         String [] columnNames = {
-                "Account Number", "Account Type", "Balance", "Customer ID"
+                "Account Number", "Account Type", "Balance", "Transfer Amount",
+                "Transfer Quantity", "Customer ID", "Open Date"
         };
-        Object[][] data = {
-                {"843592944", 1, 7698.17, 1},
-                {"196191617", 2, 20327.59, 2},
-                {"955291079", 3, 16100.18, 3},
-                {"150300551", 1, 24524.79, 4},
-                {"929240322", 2, 40429.33, 5},
-                {"348821130", 3, 65067.92, 6}
-        };
-        accountTable = new JTable(data,columnNames);
+        Object[][] data = new String[0][];
+        TableModel tableModel = new DefaultTableModel(data, columnNames);
+
+        accountTable = new JTable(tableModel);
         accountTable.setPreferredScrollableViewportSize(new Dimension(500,100));
         accountTable.setFillsViewportHeight(true);
     }
@@ -111,5 +152,23 @@ public class SearchPanel extends JPanel {
         JScrollPane AccountScrollPane = new JScrollPane(accountTable);
         AccountScrollPane.setBounds(100, 350, 800,200);
         this.add(AccountScrollPane, null);
+    }
+
+    private void updateAccountTable() {
+        DefaultTableModel tableModel = (DefaultTableModel) accountTable.getModel();
+        tableModel.setRowCount(0);
+        for (Account account : resultAccounts) {
+            String[] data = new String[tableModel.getColumnCount()];
+            data[0] = account.getAccNumber();
+            data[1] = account.getAccType();
+            data[2] = String.valueOf(account.getBalance());
+            data[3] = String.valueOf(account.getTransferAmount());
+            data[4] = String.valueOf(account.getTransferQuantity());
+            data[5] = String.valueOf(account.getCustomerId());
+            data[6] = String.valueOf(account.getOpenDate());
+            tableModel.addRow(data);
+        }
+        accountTable.setModel(tableModel);
+        accountTable.repaint();
     }
 }
